177120c6e1b1d056e87ba29633c122827e0597fd
WesleyCarlos41 <102267247+WesleyCarlos41@users.noreply.github.com>
2022-05-11 10:27:58 -0300

    Testando api

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -12,7 +12,7 @@ public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
 	//Quando não se sabe qual tipo de resposta pode ter ao buscar na requisição e tem mais de uma possível
 
-	public Optional<Usuario> findByUsuario(String usuario);
+	public Optional<Usuario>findByUsuario(String usuario);
 	
 }
 
